4337c3f0b3a94bec5ebb1f9bfe4f61288b4894c2
ranja <ranja@LAPTOP-L92RK773>
2023-09-15 14:09:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Chessboard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange en siffra! ");
+            int number = int.Parse(Console.ReadLine()); //omvandlar användarens svar till ett heltal
+
+
+            for (int i = 0; i < number; i++)
+            {
+
+
+                for (int x = 0; number > x; x = x + 1)
+                {
+                    Console.Write("0");
+                    x++;
+
+                    //Gör en if sats för att skriva ut exakt många pjäser som anges
+                    if (number > x)  // Villkor
+                    {
+                        Console.Write("X");
+                    }
+
+                }
+                Console.WriteLine(""); // ny rad efter varje loop
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
